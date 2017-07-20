6f968dff6ba0ddeab9be3539a3a345a98de4e13c
docwali777 <drgauvin@mgmcare.org>
2017-07-19 22:58:52 -0400

    server

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,8 +5,7 @@ module.exports = {
   entry: './client/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'public'),
-    publicPath: '/'
+    path: path.join(__dirname, 'public')
   },
   module: {
     rules: [
