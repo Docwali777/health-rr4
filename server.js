6135d5dbf6a4ff85606eb542dcdb0354d063bd76
docwali777 <drgauvin@mgmcare.org>
2017-07-19 23:45:34 -0400

    8

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-
 const express = require('express');
 const app = express()
 const path = require('path');
 
 app.use(express.static('public'))
 
-
 app.get('/*', (req, res)=>{
-  const filePath = path.resolve(path.join(__dirname, 'public/index.html'))
+  const filePath = path.resolve(__dirname, 'public', 'index.html')
   res.sendFile(filePath)
 })
 app.listen(3000, ()=>{
