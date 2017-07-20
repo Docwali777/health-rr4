6ec961f98a7f686f4ec9738c573e782c86ab8ba3
docwali777 <drgauvin@mgmcare.org>
2017-07-20 10:52:14 -0400

    prod

--- server.js
+++ server.js
@@ -5,30 +5,6 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
-const webpack = require('webpack')
-const webpackMiddleware = require('webpack-dev-middleware')
-const webpackHotMiddleware = require('webpack-hot-middleware')
-const config = require('./webpack.dev.js')
-
-const compiler = webpack(config)
-
-const middleware = webpackMiddleware(compiler, {
-  publicPath: config.output.publicPath,
-  contentBase: 'client',
-  noInfo: true,
-  hot: true,
-  stats: {
-    colors: true,
-    progress: true,
-    chunkModules: true
-  }
-})
-
-if(process.env.NODE_ENV === undefined){
-  app.use(middleware)
-  app.use(webpackHotMiddleware(compiler))
-}
-
 app.use(express.static('public'))
 
 app.get('/*', (req, res)=>{
