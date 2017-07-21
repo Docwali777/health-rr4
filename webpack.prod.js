2442138b576b4ee2b8e7255af1d02ac4d4a41e2c
docwali777 <drgauvin@mgmcare.org>
2017-07-20 21:27:16 -0400

    optimizing

--- webpack.prod.js
+++ webpack.prod.js
@@ -21,6 +21,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
     filename: '[name].[chunkhash].js',
+    chunkFilename: '[id].js',
     sourceMapFilename: '[name].map'
   },
   module: {
@@ -35,6 +36,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
+        exclude: /node_modules/,
         use: ExtractTextPlugin.extract({
           fallback: "style-loader",
           use: "css-loader"
@@ -42,7 +44,11 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader']
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"]
+        })
       },
       {
         test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
